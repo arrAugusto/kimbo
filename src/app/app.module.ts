6535ae6d8cb102363c1bd20392479c107bce0300
Augusto Gomez <agr120@hotmail.com>
2024-02-18 23:03:26 -0600

    ADD NAV BAR AND SIDEBAR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router'; // Importa RouterModule
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routes';
-import { HelloComponent } from './hello/hello.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent, NavBarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { RouterModule, Routes } from '@angular/router';
-import { HelloComponent } from './hello/hello.component';
 import { NgModule } from '@angular/core';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 export const routes: Routes = [
-  { path: 'hello', component: HelloComponent },
+  { path: 'hello', component: NavBarComponent },
   // Otras rutas si las tienes
 ];
 
